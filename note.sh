6e710295cfb9bb715ee595ea20a8db1cd80c0e61
jmss127 <jmss127@protonmail.com>
2025-07-22 23:01:33 +0100

    changed format, and clears terminal after each entry

--- note.sh
+++ note.sh
@@ -9,48 +9,49 @@
 
 # Config:
 Color Variables tput
-#r=$(tput setaf 1) # Warnings/errors Red
-g=$(tput setaf 2) # Menus/info/success Green
-c=$(tput setaf 6) # Input prompts Cyan
-m=$(tput setaf 5) # Highlights Magenta
+r=$(tput setaf 1) # Warnings/errors Red
+g=$(tput setaf 2)  # Menus/info/success Green
+c=$(tput setaf 6)  # Input prompts Cyan
+m=$(tput setaf 5)  # Highlights Magenta
 reset=$(tput sgr0) # Reset
 
 trap 'echo -e "$reset"; exit 0' INT TERM EXIT
 DATE=$(date "+## %a %d %b %Y %R")
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Resolve script's directory
-FILE="note.md"                  # set markdown file name
-NOTE="$SCRIPT_DIR/$FILE"        # Script dir and markdown file
-
+FILE="note.md"                                             # set markdown file name
+NOTE="$SCRIPT_DIR/$FILE"                                   # Script dir and markdown file
 
 # Main:
-clear
 if [ ! -f "$NOTE" ]; then
     touch "$NOTE"
 fi
 # getopts -r for read note
 while getopts "r" flag; do
-case ${flag} in
-    r) nano -v "$NOTE"
+    case ${flag} in
+    r)
+        nano -v "$NOTE"
         exit 0
-    ;;
-    *) exit 0
-    ;;
-esac
+        ;;
+    *)
+        exit 0
+        ;;
+    esac
 done
+while true; do
+    clear
+    echo "${g}** ${m}Note ${g}**${reset}"
+    echo
+    # Enter title
+    read -r -p "${g}Enter Title or ${m}CTRL+C${g} to exit ${c}> ${reset}" title1
+    echo
+    echo "${g}Write your note and save with ${m}Enter${reset}"
+    echo
+    # read input with line editor
+    echo -ne "$c"
+    read -e -r -p "> ${reset}" note1
+    echo -e "$reset"
 
-echo "${g}** ${m}Note ${g}**${reset}"
-echo 
-# Enter title
-read -r -p "${g}Enter Title ${c}> ${reset}" title1
-echo 
-echo "${g}Write your note and save with ${m}Enter${reset}"
-echo 
-# read input with line editor
-echo -ne "$c"
-read -e -r -p "> ${reset}" note1
-echo -ne "$reset"
-
-cat >> "$NOTE" << EOF
+    cat >>"$NOTE" <<EOF
 $DATE
 
 ### $title1
@@ -60,4 +61,5 @@ $note1
 ___
 
 EOF
+done
 exit 0
