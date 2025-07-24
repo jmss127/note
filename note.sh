b75f465881aa08849336eb8092c32fd760ee32f0
jmss127 <jmss127@protonmail.com>
2025-07-24 17:27:14 +0100

    added search option with bat and awk

--- note.sh
+++ note.sh
@@ -7,7 +7,7 @@
 # append entries to file.
 
 # Config:
-Color Variables tput
+#Color Variables tput
 # r=$(tput setaf 1) # Warnings/errors Red
 g=$(tput setaf 2)  # Menus/info/success Green
 c=$(tput setaf 6)  # Input prompts Cyan
@@ -15,18 +15,24 @@ m=$(tput setaf 5)  # Highlights Magenta
 reset=$(tput sgr0) # Reset
 
 trap 'echo -e "$reset"; exit 0' INT TERM EXIT
-DATE=$(date "+## %a %d %b %Y %R")
+DATE=$(date "+%a %d %b %Y %R")
 SCR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Resolve script's directory
 FILE="note.md"                                          # set markdown file name
 NOTE="$SCR_DIR/$FILE"                                   # Script dir and markdown file
 
 # Main:
 [ ! -f "$NOTE" ] && touch "$NOTE"
-# getopts -r for read note
-while getopts "r" flag; do
+# getopts -r for read note, s: for searching
+while getopts "rs:" flag; do
     case ${flag} in
     r) nano -v "$NOTE"; exit 0; ;;
-    *) exit 0 ;;
+    # Select a suitable pager here.
+    s) awk -v w="${OPTARG}" '
+        /^##/ {record=""; inblock=1}
+        /^___/ {inblock=0; if (record ~ w) print record}
+        inblock {record = record $0 "\n"}' "$FILE" | batcat --file-name "$NOTE"
+        exit 0; ;; 
+    *) exit 0; ;;
     esac
 done
 while true; do
@@ -35,7 +41,7 @@ while true; do
     echo
     echo "${g}Write your note and save with ${m}Ctrl+D${g} on a new line or ${m}Ctrl+C${g} to exit${reset}"
     echo
-    # read input with line editor
+    # read input
     echo -ne "$c> ${reset}"
     note_in=$(cat) # capture multiline input
     echo -e "$reset"
@@ -44,9 +50,8 @@ while true; do
     # Append to file using ed
     ed -s "$NOTE" <<EOF
 \$a
-$DATE
 
-### $title
+## $title - $DATE
 
 $note_in
 
